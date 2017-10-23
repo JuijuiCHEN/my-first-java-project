701e306439ca2873247af6552cd6d8bd1b7cf337
瑞瑞 <@192.168.0.100>
2017-10-24 00:16:27 +0800

    基礎java(1)

---
+++ my-first-java-project/src/com/jui/helloJava/Basis.java
@@ -0,0 +1,33 @@
+package com.jui.helloJava;
+
+public class Basis {
+
+	public static void main(String[] args) {
+
+		//字串
+		String hi = "狗";
+		String clara = "貓";
+		System.out.println(hi);
+		System.out.println(clara);
+		
+		//數字(正整數)
+		int one = 1;
+		System.out.println(one);
+		
+		//數字(小數點)
+		double d = 0.02;
+		double number = 2;
+		System.out.println(d);
+		System.out.println(number);
+		
+		
+		//布林(存放正確或不正確)
+		boolean t = true;
+		boolean f = false;
+		
+			
+		
+		
+	}
+
+}
